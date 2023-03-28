4ed586a1150c1ba0ff441f201158e68513c7af58
LeJammes454 <s19120178@alumnos.itsur.edu.mx>
2023-03-28 09:36:07 -0600

    agregando logica para evitar que el balon se salga dela pantalla  50/50 aun se sale un poco en la direccion este y sur

--- app/src/main/java/com/example/proyectosensores/MainActivity.kt
+++ app/src/main/java/com/example/proyectosensores/MainActivity.kt
@@ -6,6 +6,7 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.ImageView
 
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var ballImage: ImageView
 
+    private lateinit var displayMetrics: DisplayMetrics
+    private var ballWidth: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +27,9 @@ class MainActivity : AppCompatActivity() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-
         ballImage = findViewById(R.id.ballImage)
+        displayMetrics = resources.displayMetrics
+        ballWidth = ballImage.width
     }
     private val sensorListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent) {
@@ -33,11 +37,24 @@ class MainActivity : AppCompatActivity() {
             val y = event.values[1]
             Log.d("ACCELEROMETER", "x: $x, y: $y")
 
-
             val newX = ballImage.translationX - x
             val newY = ballImage.translationY + y
-            ballImage.translationX = newX
-            ballImage.translationY = newY
+
+            if (newX < 0) {
+                ballImage.translationX = 0f
+            } else if (newX + ballWidth > displayMetrics.widthPixels) {
+                ballImage.translationX = (displayMetrics.widthPixels - ballWidth).toFloat()
+            } else {
+                ballImage.translationX = newX
+            }
+
+            if (newY < 0) {
+                ballImage.translationY = 0f
+            } else if (newY + ballWidth > displayMetrics.heightPixels) {
+                ballImage.translationY = (displayMetrics.heightPixels - ballWidth).toFloat()
+            } else {
+                ballImage.translationY = newY
+            }
         }
 
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {

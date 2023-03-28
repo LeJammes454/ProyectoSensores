f175e0d6ac8cce4fd855758d47ca71765e959b24
LeJammes454 <s19120178@alumnos.itsur.edu.mx>
2023-03-28 09:30:14 -0600

    agrenado balon y quitanto la barra superiror

--- app/src/main/java/com/example/proyectosensores/MainActivity.kt
+++ app/src/main/java/com/example/proyectosensores/MainActivity.kt
@@ -7,11 +7,14 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sensorManager: SensorManager
     private lateinit var accelerometer: Sensor
+
+    private lateinit var ballImage: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +23,21 @@ class MainActivity : AppCompatActivity() {
 
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
+        ballImage = findViewById(R.id.ballImage)
     }
     private val sensorListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent) {
             val x = event.values[0]
             val y = event.values[1]
             Log.d("ACCELEROMETER", "x: $x, y: $y")
+
+
+            val newX = ballImage.translationX - x
+            val newY = ballImage.translationY + y
+            ballImage.translationX = newX
+            ballImage.translationY = newY
         }
 
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {

b3111abe1e526d90b81d72abefc6ed0840e1d9ee
LeJammes454 <s19120178@alumnos.itsur.edu.mx>
2023-03-27 23:41:03 -0600

    comit inicial

---
+++ app/src/main/java/com/example/proyectosensores/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.proyectosensores
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+    private val sensorListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            Log.d("ACCELEROMETER", "x: $x, y: $y")
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            // No necesitamos implementar esto
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(sensorListener)
+    }
+
+
+}
